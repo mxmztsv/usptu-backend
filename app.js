588ae850df5548b25967d286ab28d8768e507449
RussianHustla <maximilyn10@mail.ru>
2022-06-03 23:02:26 +0300

    cors origin ip changed

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const app = express();
  */
 const corsOptions = {
     // origin: 'http://localhost:3000',
-    origin: 'http://192.168.0.104:3000',
+    origin: 'http://192.168.0.107:3000',
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     credentials: true
 }
